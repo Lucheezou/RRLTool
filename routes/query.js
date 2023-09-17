ea0779fcb8bee6646a83cdf7fd48faaff6a008d0
Lucheezou <lukevincent4@gmail.com>
2023-09-18 01:56:04 +0800

    added selection

--- routes/query.js
+++ routes/query.js
@@ -12,19 +12,16 @@ const model = new openai({
 /* GET users listing. */
 router.post('/', async function(req, res, next) {
 
-async function getQuery(question){
+async function getQuery(question,thesis){
 const chatCompletion = await model.chat.completions.create({
-    messages: [{ role: "user", content: `respond with just a json object with keys 1-5 and values containing a concise paragraph answer to these questions [research objective for a research called ${question},  what is the methodology for a research called ${question}, what are the key findings for a research called ${question}, what is the relevance for a research called  ${question} to a thesis called Named Entity Recognition for Tagalog using Transformers, what is the difference for a research called  ${question} to a thesis called Named Entity Recognition for Tagalog using Transformers]` }],
+    messages: [{ role: "user", content: `respond with just a json object with keys 1-5 and values containing a concise paragraph answer to these questions [research objective for a research called ${question},  what is the methodology for a research called ${question}, what are the key findings for a research called ${question}, what is the relevance for a research called  ${question} to a thesis called ${thesis} using Transformers, what is the difference for a research called  ${question} to a thesis called ${thesis}]` }],
     model: "gpt-3.5-turbo",
 });
-
-
-
 
 return chatCompletion
 }
 
-  let response = await getQuery(req.body.question)
+  let response = await getQuery(req.body.question,req.body.thesis)
   console.log(response.choices[0].message.content)
   res.json(response.choices[0].message.content)
 
